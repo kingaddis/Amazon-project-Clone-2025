80e33d5aebb220fd2b19e4f355b999656242c1fa
kingaddis <66341899+kingaddis@users.noreply.github.com>
2025-06-03 12:49:56 -0400

    Fix base URL for Netlify and update backend

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
 
 // https://vite.dev/config/
 // export default defineConfig({
@@ -8,8 +7,11 @@ import react from '@vitejs/plugin-react'
 //     ? "/Amazon-project-Clone-2025/" 
 //     : "/",
 // })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // ✅ Always use '/' for Netlify
+  base: '/',
+  publicDir: 'public', // Ensure this points to your public folder
 })
