2890e71874d81129329c2d05225b0532f83e727d
kingaddis <66341899+kingaddis@users.noreply.github.com>
2025-06-03 12:59:56 -0400

    Fix base URL for Netlify and update backend

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/',
+//   publicDir: 'public', // Ensure this points to your public folder
+// })
+
 export default defineConfig({
-  plugins: [react()],
-  base: '/',
-  publicDir: 'public', // Ensure this points to your public folder
+  plugins: [react({
+    // Auto-inject script in dev mode
+    jsxRuntime: 'automatic',
+  })],
 })
