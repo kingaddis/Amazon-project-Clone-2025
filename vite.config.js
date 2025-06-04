7bb4ded1a69b6ff22f7a4a63d4723ca7ff74e303
kingaddis <66341899+kingaddis@users.noreply.github.com>
2025-06-04 11:08:14 -0400

    Fix base 10

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,4 @@
 
-
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: process.env.NODE_ENV === "production" 
-//     ? "/Amazon-project-Clone-2025/" 
-//     : "/",
-// })
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '/',
-//   publicDir: 'public', // Ensure this points to your public folder
-// })
-
-// export default defineConfig({
-//   plugins: [react({
-//     // Auto-inject script in dev mode
-//     jsxRuntime: 'automatic',
-//   })],
-// })
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
